9013d0726fef9c0d3b16ab17db521d12189211dc
Crowdbotics <team@crowdbotics.com>
2023-07-24 08:17:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-sea-42379",
+  url: "https://aged-sea-42379.botics.co",
+  api: "https://aged-sea-42379.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
